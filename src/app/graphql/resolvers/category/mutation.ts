aee0328e8c5f6b14b9517a98c256ea51a1e50471
Joseph Logan <jarm061198@gmail.com>
2021-03-28 16:45:33 -0600

    Category module finished

--- src/app/graphql/resolvers/category/mutation.ts
+++ src/app/graphql/resolvers/category/mutation.ts
@@ -1,19 +1,37 @@
-import { Models } from '@app/models'
+import { Models } from '@app/models';
 import { IResolvers } from 'graphql-tools';
 import Logger from '@loaders/logger';
 
 const mutation: IResolvers = {
   Mutation: {
-    storeCategory: async (_: void, {category}) => {
+    storeCategory: async (_: void, { category }) => {
       try {
         let categoryInstance = new Models.Category(category);
-        return await categoryInstance.save()
+        return await categoryInstance.save();
       } catch (err) {
-        Logger.error('Error in %o: ', err)
-        throw err
+        Logger.error('Error in %o: ', err);
+        throw err;
+      }
+    },
+    updateCategory: async (_: void, {_id, category}) => {
+      try {
+        return await Models.Category.findByIdAndUpdate(_id, category , {
+          new: true
+        });
+      } catch (err) {
+        Logger.error('Error in %o: ', err);
+        throw err;
+      }
+    },
+    deleteCategory: async (_: void, {_id}) => {
+      try {
+        return await Models.Category.findByIdAndDelete(_id);
+      } catch (err) {
+        Logger.error('Error in %o: ', err);
+        throw err;
       }
     }
-  }
-}
+  },
+};
 
-export default mutation
+export default mutation;
--- src/app/graphql/resolvers/category/query.ts
+++ src/app/graphql/resolvers/category/query.ts
@@ -9,6 +9,15 @@ const query: IResolvers = {
         return await Models.Category.find()
       } catch (err) {
         Logger.error('Error in %o: ', err)
+        throw err
+      }
+    },
+    category: async (_: void, {_id}) => {
+      try {
+        return await Models.Category.findById(_id)
+      } catch (err) {
+        Logger.error('Error in %o: ', err)
+        throw err
       }
     }
   }
